b4599bb3521d4b60d189eb97ceac1170c852210a
roberto <usr2266@salesianos-lacuesta.net>
2022-01-21 14:17:02 +0000

    New feature: "Main updated, now the user will be able to decide the fair size"

--- src/com/company/fairmanager/Main.java
+++ src/com/company/fairmanager/Main.java
@@ -2,11 +2,19 @@ package com.company.fairmanager;
 
 import com.company.fair.FairManager;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        int numVehicles = 10;
-        int numSpaces = 10;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Introduce el numero de vehiculos");
+        int numVehicles = scan.nextInt();
+
+        System.out.println("Introduce el numero de espacios para la feria");
+        int numSpaces = scan.nextInt();
         FairManager fair = new FairManager(numVehicles, numSpaces);
     }
 }
